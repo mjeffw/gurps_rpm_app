f1f63f4c8be51eb9bac6c62519f5385bc2ba1b18
Wilson, Marvin (jw9615) <jw9615@att.com>
2020-10-21 17:03:47 -0400

    got model binding tests working

--- test/widgets/provider_aware_textfield_test.dart
+++ test/widgets/provider_aware_textfield_test.dart
@@ -45,6 +45,11 @@ void main() {
                     },
                   ),
                 ),
+                Expanded(
+                  child: TextField(
+                    key: Key('DUMMY'),
+                  ),
+                )
               ],
             ),
           ),
@@ -55,7 +60,48 @@ void main() {
       expect(find.byKey(Key('Field-TEXT')), findsOneWidget);
 
       await tester.enterText(find.byKey(Key('Field-TEXT')), 'FOO');
+
+      // move the focus somewhere else... anywhere else
+      await tester.showKeyboard(find.byKey(Key('DUMMY')));
       expect(model.name, equals('FOO'));
+    });
+
+    testWidgets('should update when model changes',
+        (WidgetTester tester) async {
+      _Model model = _Model();
+
+      await tester.pumpWidget(ChangeNotifierProvider(
+        create: (_) => model,
+        builder: (_, __) => MaterialApp(
+          home: Card(
+            child: Column(
+              children: [
+                Expanded(
+                  child: ProviderSelectorTextField<_Model>(
+                    key: Key('Field'),
+                    valueProvider: (context, model) => (model as _Model).name,
+                    onSubmitted: (value) {
+                      return model.name = value;
+                    },
+                  ),
+                ),
+                Expanded(
+                  child: TextField(
+                    key: Key('DUMMY'),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ));
+
+      expect(model.name, equals('fee'));
+      model.name = 'BAR';
+
+      // move the focus somewhere else... anywhere else
+      await tester.showKeyboard(find.byKey(Key('DUMMY')));
+      expect(model.name, equals('BAR'));
     });
   });
 }
